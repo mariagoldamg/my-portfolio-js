22a67df87eb0e72765c7ea29032feecbc392d8ca
mariagoldamg <mariamelnikova88@gmail.com>
2023-04-11 12:01:55 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,145 @@
+AOS.init();
+ 
+ particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 30,
+        "density": {
+          "enable": true,
+          "value_area": 100
+        }
+      },
+      "color": {
+        "value": "#B9E0FF"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 7
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+  
+
+  gsap.to ("#frontEnd", {
+    delay:2,
+    text:"web developer",
+    duration:2.5,
+   
+    ease:"linear",
+})
+gsap.from ("img", {
+   
+    opacity:0,
+    delay:4.7,
+    duration:2
+})
+gsap.from (".fs-5", {
+    y:-100,
+     opacity:0,
+     delay:.5,
+     duration:2,
+     
+ })
+ gsap.from ("p", {
+    opacity:0,
+    delay:1.5,
+    duration:2
+})
+gsap.from ("h1", {
+  opacity:0,
+  delay:5.2,
+  duration:2
+})
